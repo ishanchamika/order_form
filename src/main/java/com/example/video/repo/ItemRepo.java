a3c7abe6a20d26ae4d5375f46338ee1096905cde
ishanchamika <ishanchami9@gmail.com>
2024-10-15 23:21:19 +0530

    item repo

---
+++ src/main/java/com/example/video/repo/ItemRepo.java
@@ -0,0 +1,13 @@
+package com.example.video.repo;
+
+import com.example.video.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@EnableJpaRepositories
+@Repository
+public interface ItemRepo extends JpaRepository<Item , Integer>
+{
+
+}
