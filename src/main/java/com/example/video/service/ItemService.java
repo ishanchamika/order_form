ad14d291a793d9259065312cec99d2d52e505d22
ishanchamika <ishanchami9@gmail.com>
2024-10-18 22:08:07 +0530

    update

--- src/main/java/com/example/video/service/ItemService.java
+++ src/main/java/com/example/video/service/ItemService.java
@@ -1,8 +1,13 @@
 package com.example.video.service;
 
+import com.example.video.dto.ItemDTO;
 import com.example.video.dto.request.RequestSaveItemDTO;
+
+import java.util.List;
 
 public interface ItemService
 {
     void addItem(RequestSaveItemDTO requestSaveItemDTO);
+
+    List<ItemDTO> getItemByNameAndActiveState(String itemName);
 }
