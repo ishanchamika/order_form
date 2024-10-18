c314601b1e0b4b4e92d6a899c4dc8ca3e21bc391
ishanchamika <ishanchami9@gmail.com>
2024-10-18 22:10:33 +0530

    update

--- src/main/java/com/example/video/service/impl/ItemServiceIMPL.java
+++ src/main/java/com/example/video/service/impl/ItemServiceIMPL.java
@@ -5,9 +5,12 @@ import com.example.video.dto.request.RequestSaveItemDTO;
 import com.example.video.entity.Item;
 import com.example.video.repo.ItemRepo;
 import com.example.video.service.ItemService;
+import com.example.video.util.mappers.ItemMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemServiceIMPL implements ItemService
@@ -18,14 +21,35 @@ public class ItemServiceIMPL implements ItemService
   @Autowired
   private ModelMapper modelMapper;
 
+
+  @Autowired
+  private ItemMapper itemMapper;
+
   @Override
   public void addItem(RequestSaveItemDTO requestSaveItemDTO)
   {
-    Item item = modelMapper.map(requestSaveItemDTO, Item.class);
+//    Item item = modelMapper.map(requestSaveItemDTO, Item.class);
+    Item item = itemMapper.requestDtoToEntity(requestSaveItemDTO);
     item.setActiveState(false);
     if(!itemRepo.existsById(item.getItemId()))
     {
       itemRepo.save(item);
     }
   }
+
+  public List<ItemDTO> getItemByNameAndActiveState(String itemName)
+  {
+    if(itemName != null)
+    {
+      int b= 1;
+      List<Item> items = itemRepo.findAllByItemName(itemName);
+      List<ItemDTO> itemDTOList = itemMapper.entityListToDtoList(items);
+      return itemDTOList;
+    }
+    else
+    {
+      return null;
+    }
+  }
+
 }
---
+++ src/main/java/com/example/video/util/StandardResponse.java
@@ -0,0 +1,4 @@
+package com.example.video.util;
+
+public class StandardResponse {
+}
