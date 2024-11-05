a4248b01b78321e89615c1d6a43193853a8dad7c
ishanchamika <ishanchami9@gmail.com>
2024-11-05 13:06:30 +0530

    create pageToList mapper

--- src/main/java/com/example/video/util/mappers/ItemMapper.java
+++ src/main/java/com/example/video/util/mappers/ItemMapper.java
@@ -4,6 +4,7 @@ import com.example.video.dto.ItemDTO;
 import com.example.video.dto.request.RequestSaveItemDTO;
 import com.example.video.entity.Item;
 import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -13,4 +14,5 @@ public interface ItemMapper
     Item requestDtoToEntity(RequestSaveItemDTO requestSaveItemDTO);
 //    List<Item> requestDtoListToEntityList(List<RequestSaveItemDTO> requestSaveItemDTO);
     List<ItemDTO> entityListToDtoList(List<Item>items);
+    List<ItemDTO> pageToList(Page<Item> items);
 }
