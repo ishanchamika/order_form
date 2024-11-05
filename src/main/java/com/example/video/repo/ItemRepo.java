6419db6b9bdad76ffaeb45b94313d81676aec228
ishanchamika <ishanchami9@gmail.com>
2024-11-05 13:07:44 +0530

    create method findAllByActiveStateEquals and countAllByActiveStateEquals to get paginated response and activeItem Count

--- src/main/java/com/example/video/repo/ItemRepo.java
+++ src/main/java/com/example/video/repo/ItemRepo.java
@@ -1,6 +1,9 @@
 package com.example.video.repo;
 
 import com.example.video.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
@@ -12,4 +15,10 @@ import java.util.List;
 public interface ItemRepo extends JpaRepository<Item , Integer>
 {
     List<Item> findAllByItemName(String itemName);
+
+    List<Item> findAllByActiveStateIs(boolean b);
+
+    Page<Item> findAllByActiveStateEquals(boolean activeState, Pageable pageable);
+
+    int countAllByActiveStateEquals(boolean activeState);
 }
